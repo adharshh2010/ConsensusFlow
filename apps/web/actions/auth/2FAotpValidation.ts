9ed64cb2573ec6a934fd62bfa0a0efe60a10927f
adharsh <lordgamingyt648@gmail.com>
2026-03-12 13:27:59 +0530

    otp final

--- apps/web/actions/auth/2FAotpValidation.ts
+++ apps/web/actions/auth/2FAotpValidation.ts
@@ -4,7 +4,9 @@ import * as z from "zod";
 
 import { OTPSchema } from "@/schema/auth";
 
-import { db, getUserByEmail } from "@workspace/consensusflow";
+import { db, getUserByEmail, otptable } from "@workspace/consensusflow";
+import { cookies } from "next/headers";
+import { eq } from "drizzle-orm";
 
 export const verify2FAotp = async (otp: z.infer<typeof OTPSchema>) => {
   try {
@@ -15,6 +17,23 @@ export const verify2FAotp = async (otp: z.infer<typeof OTPSchema>) => {
         error: "Invalid OTP format. OTP must be a 6-digit number.",
       };
     }
+
+    const remember = (await cookies()).get("remember")?.value === "true";
+
+    (await cookies()).delete("remember");
+    (await cookies()).delete("email");
+
+    const field = db.query.otptable.findFirst({
+      where: eq(otptable.otp, DataValidation.data.otp),
+    });
+
+    const email = await field.then((res) => res?.email);
+
+    const user = await getUserByEmail(email!);
+
+    // Add session etc here
+
+    await db.delete(otptable).where(eq(otptable.otp, DataValidation.data.otp));
 
     return {
       success: "OTP verified successfully. You are now logged in.",
